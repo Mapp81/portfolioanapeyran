58ab7341626ae3da8e5c9981cdae9f5d2b6101e6
Mapp81 <anapeyran@gmail.com>
2025-12-01 18:33:13 -0300

    primera subida

---
+++ portfolio/public/script.js
@@ -0,0 +1,10 @@
+const prender= document.querySelector(".prender");
+const apagar= document.querySelector(".apagar");
+
+prender.addEventListener("click", ()=>{
+    document.classlist.toggle("cambiar")
+})
+
+
+
+ 
